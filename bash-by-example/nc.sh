a787342c728b4c0a81934e79bed27c236874e21a
rolniuq <truongquynh2525@gmail.com>
2025-09-10 17:38:43 +0700

    chore: 10 Sep

---
+++ bash-by-example/nc.sh
@@ -0,0 +1,9 @@
+# The nc command, also known as netcat, is a versatile networking utility for reading from and writing to network connections using TCP or UDP. It can be used for a variety of purposes, including:
+
+# •    Port scanning: Checking for open ports on a remote host.
+# •    Banner grabbing: Retrieving service banners to identify running services.
+# •    File transfer: Sending and receiving files between hosts.
+# •    Chatting: Creating simple client-server chat applications.
+# •    Proxying: Redirecting traffic from one port to another.
+
+nc -zv google.com 443
---
+++ bash-by-example/tee.sh
@@ -0,0 +1,5 @@
+# once run this command the text Hi im tee will be append
+echo "#Hi im tee" | tee -a tee.sh
+#Hi im tee
+#Hi im tee
+#Hi im tee
